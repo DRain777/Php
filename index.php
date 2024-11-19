d57c61a3da702c2476235b8610a09bb2ff139998
DRain777 <d.cochew777@gmail.com>
2024-11-19 16:22:48 +0700

    lesson_5

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require 'header.php';
+
+    if (file_exists('header.php')) {
+        require_once 'header.php';
+    } else {
+        echo "Файл header.php не найден.";
+    }
+
+    function sum($a, $b)
+    {
+        print $a + $b;
+    };
+
+    sum(7, 7);
+
+    ?>
+
+</body>
+
+</html>
